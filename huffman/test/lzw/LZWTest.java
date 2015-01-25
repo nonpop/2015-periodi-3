76da037bf73e2deb63b5084c99e4422d54e8dd9d
kaappiloota <nonpop@kaappiloota>
2015-01-26 00:47:21 +0200

    removed unnecessary test init methods

--- huffman/test/lzw/LZWTest.java
+++ huffman/test/lzw/LZWTest.java
@@ -2,10 +2,6 @@ package lzw;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
