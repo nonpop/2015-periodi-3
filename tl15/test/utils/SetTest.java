0510f65bbba31f5e140557ee365298468781ada2
kaappiloota <nonpop@kaappiloota>
2015-02-05 21:36:55 +0200

    a little set test

---
+++ tl15/test/utils/SetTest.java
@@ -0,0 +1,22 @@
+package utils;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SetTest {
+    @Test
+    public void test() {
+        Set s = new Set(3);
+        s.put(3);
+        s.put(2);
+        s.put(3);
+        s.put(-10);
+        assertTrue(s.contains(2));
+        assertTrue(s.contains(3));
+        assertTrue(s.contains(-10));
+        s.clear();
+        assertFalse(s.contains(2));
+        assertFalse(s.contains(3));
+        assertFalse(s.contains(-10));
+    }
+}
