68c8140be4ba35ceabb557ba8b9b3bafef1c00be
kaappiloota <nonpop@kaappiloota>
2015-02-05 21:25:02 +0200

    some trivial list tests

--- tl15/test/utils/ListTest.java
+++ tl15/test/utils/ListTest.java
@@ -38,5 +38,68 @@ public class ListTest {
         assertEquals(false, v.get(0));
         v.removeFirst();
         assertEquals(0, v.size());
+        v.add(1);
+        v.add(2);
+        v.removeLast();
+        assertEquals(1, v.size());
+        assertEquals(1, v.get(0));
+        v.add(1);
+        v.add(2);
+        v.clear();
+        assertEquals(0, v.size());
+        v.clear();
+        assertEquals(0, v.size());
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testGetBadEmpty() {
+        List v = new List();
+        v.get(0);
+    }
+    
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testGetBadSmall() {
+        List v = new List();
+        v.add(0);
+        v.get(-1);
+    }
+    
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testGetBadBig() {
+        List v = new List();
+        v.add(0);
+        v.get(1);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testSetBadEmpty() {
+        List v = new List();
+        v.set(0, 0);
+    }
+    
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testSetBadSmall() {
+        List v = new List();
+        v.add(0);
+        v.set(-1, 0);
+    }
+    
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testSetBadBig() {
+        List v = new List();
+        v.add(0);
+        v.set(1, 0);
+    }
+    
+    @Test(expected = IllegalStateException.class)
+    public void testRemoveFirstBad() {
+        List v = new List();
+        v.removeFirst();
+    }
+    
+    @Test(expected = IllegalStateException.class)
+    public void testRemoveLastBad() {
+        List v = new List();
+        v.removeLast();
     }
 }
