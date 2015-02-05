59544f0cff608ae96c8e67df5620700b92aa087f
kaappiloota <nonpop@kaappiloota>
2015-02-05 02:14:09 +0200

    a min function

--- tl15/src/utils/Math.java
+++ tl15/src/utils/Math.java
@@ -12,4 +12,8 @@ public class Math {
     public static int max(int a, int b) {
         return (a > b)? a : b;
     }
+
+    public static int min(int a, int b) {
+        return (a < b)? a : b;
+    }
 }
